992425c12fcefb7ec9beb247bde06d21bda4686f
SteelER1337 <frederik.gladishev@gmail.com>
2020-11-10 14:10:27 +0300

    lab1 template post.html

--- lab1/app/app.py
+++ lab1/app/app.py
@@ -1,17 +1,54 @@
+from random import randint
 from flask import Flask, render_template, url_for
+from faker import Faker
+
+fake=Faker()
 
 app = Flask(__name__)  
 application = app
 
+images_ids = ['4jh3k5h4-fk4k-l8ly-gkks-f7gjs83nf83j',
+              'd8g88s3j-skak-33d1-d134-12vg4tqs5gs9',
+              'f83j3igi-rj4n-fn41-412s-2kfnqkfj2hr3',
+              '5k2k4n4k-5h31-gj48-skw4-3n4bnkb3l4b1',
+              '54j2k2hb-r314-5315-qrq1-53f04o2jtj1j']
+
+def generate_comments(replies=True):
+    comments=[]
+    for i in range (randint(1, 3)):
+        comment = {'author' : fake.name(), 'text' : fake.text()}
+        if replies:
+            comment['replies'] = generate_comments(replies=False)
+    return comments
+
+def generate_post(i):
+    return {
+        'title':'Заголовок поста',
+        'text': fake.paragraph(nb_sentences=100),
+        'author' : fake.name(),
+        'date': fake.date_time_between(start_date='-2y', end_date='now'),
+        'image_filename' : f'{images_ids[i]}.jpg',
+        'comments' : generate_comments()
+    }
+
+posts_list = [generate_post(i) for i in range(5) ]
+
 @app.route('/')
 def index():
-    msg = 'Hello'
-    return render_template('index.html', msg=msg) #передаём параметр msg и в качестве значения передадим значение этой переменной
+    return render_template('index.html') 
 
 @app.route('/posts')
 def posts():
-    return render_template('posts.html')
+    title = 'Посты'
+    return render_template('posts.html', title=title, posts=posts_list)
+
+@app.route('/posts/<int:index>')
+def post(index):
+    p=posts_list[index]
+    return render_template('post.html', title=p['title'], post=p)
+
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    title = 'Об авторе'
+    return render_template('about.html', title=title)
